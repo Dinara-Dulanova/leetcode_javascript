69b86ddce1a67e8732fc34b420aed34c86ac4a6e
dulanova <dinara-dulanova@yandex.ru>
2025-07-09 12:33:47 +0300

    linked list

---
+++ linked_list/21. Merge Two Sorted Lists.js
@@ -0,0 +1,26 @@
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode(0);
+    let head = dummy;
+
+    // while (list1 != null || list2 != null) {
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            dummy.next = list1;
+            list1 = list1.next
+
+        } else {
+            dummy.next = list2
+            list2 = list2.next
+        }
+
+        dummy = dummy.next
+    }
+
+    if (list1 == null) {
+        dummy.next = list2
+    } else {
+        dummy.next = list1
+    }
+
+    return head.next
+};
